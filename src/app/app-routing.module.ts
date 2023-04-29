833c964bad6ed3957aebf099f0ff0745245b7eb2
unknown <s.sabashvili12345@gmail.com>
2023-04-29 18:50:38 +0400

    header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UsersComponent } from './users/users.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { StatusesComponent } from './statuses/statuses.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/users', pathMatch: 'full' },
+  { path: 'users', component: UsersComponent },
+  { path: 'categories', component: CategoriesComponent },
+  { path: 'statuses', component: StatusesComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
